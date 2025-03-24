24b4687711d781dc61d2ed78b1931d7e7eb0f24c
Geony <jvckigh@gmail.com>
2025-03-24 22:40:38 +0900

    공연 요금 계산로직 - 함수 추출하기

--- src/main/java/gbeea/refactoring/domain/TheaterCompany.java
+++ src/main/java/gbeea/refactoring/domain/TheaterCompany.java
@@ -18,25 +18,7 @@ public class TheaterCompany {
                     .filter(p -> performance.getPlayId().equals(p.getId()))
                     .findFirst()
                     .orElseThrow(IllegalArgumentException::new);
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (performance.getAudience() > 30) {
-                        thisAmount += 1000 * (performance.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (performance.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-                    }
-                    thisAmount += 300 * performance.getAudience();
-                    break;
-                default:
-                    throw new IllegalArgumentException("알 수 없는 장르: " + play.getName());
-            }
+            int thisAmount = amountFor(performance, play);
 
             // 포인트 적립
             volumeCredits += Math.max(performance.getAudience() - 30, 0);
@@ -51,5 +33,27 @@ public class TheaterCompany {
         result += "총액: " + numberFormat.format(totalAmount / 100) + "\n";
         result += "적립 포인트: " + volumeCredits + "점\n";
         return result;
+    }
+
+    private int amountFor(Performance performance, Play play) {
+        int thisAmount;
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (performance.getAudience() > 30) {
+                    thisAmount += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (performance.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                thisAmount += 300 * performance.getAudience();
+                break;
+            default:
+                throw new IllegalArgumentException("알 수 없는 장르: " + play.getName());
+        }
+        return thisAmount;
     }
 }
